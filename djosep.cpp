e98fe8479502bfa98c098e55d775b588d88f6c8e
Daniel Josep <djosep@csub.edu>
2021-11-18 13:21:08 -0800

    Changed Spacing

--- djosep.cpp
+++ djosep.cpp
@@ -16,17 +16,17 @@ extern Bullet b;
 
 void show_Daniels_credits(int x, int y)
 {
-	Rect r;
-	r.bot = y;
-	r.left = x;
-	r.center = 1;
-	ggprint8b(&r, 16, 0x00ff0000, "Daniel Josep");
+    Rect r;
+    r.bot = y;
+    r.left = x;
+    r.center = 1;
+    ggprint8b(&r, 16, 0x00ff0000, "Daniel Josep");
 }
 
 void instructions()
 {
-	Rect r;
-	r.bot = gl.yres - 30;
+    Rect r;
+    r.bot = gl.yres - 30;
     r.left = 20;
     r.center = 0;
     ggprint8b(&r, 16, 0xfbfbfa, "Press s to show instructions");
@@ -34,8 +34,8 @@ void instructions()
 
 void show_scores(int score)
 {
-	Rect r;
-	r.bot = gl.yres - 690;
+    Rect r;
+    r.bot = gl.yres - 690;
     r.left = (gl.xres/2) - 65;
     r.center = 0;
     ggprint13(&r, 16, 0xfbfbfa, "      <%0.4i>      ", score);
--- mod.cpp
+++ mod.cpp
@@ -48,10 +48,10 @@ int height = scrn->height;
 int width  = scrn->width;
 
 Global::Global() {
-	xres = 1000;
-	yres = 700;
-	memset(keys, 0, 65536);
-	show_credits = 0;
+    xres = 1000;
+    yres = 700;
+    memset(keys, 0, 65536);
+    show_credits = 0;
     show_instructions = 0;
 };
 
@@ -349,33 +349,33 @@ int check_keys(XEvent *e) {
 
 void shoot_bullets() 
 {
-	//move shoot
-	cout << "space bar" << endl;
-	struct timespec bt;
-	clock_gettime(CLOCK_REALTIME, &bt);
-	double ts = timeDiff(&g.bulletTimer, &bt);
-	Bullet *b = &g.barr[g.nbullets];
-	if (ts > 0.1) {
-		timeCopy(&g.bulletTimer, &bt);
-		if (g.nbullets < MAX_BULLETS) {
-			timeCopy(&b->time, &bt);
-			b->pos[0] = g.ship.pos[0];
-			b->pos[1] = g.ship.pos[1];
-			b->vel[0] = g.ship.vel[0];
-			b->vel[1] = g.ship.vel[1];
-			Flt rad = ((g.ship.angle+90.0)/ 360.0f) * PI * 2.0;
-			Flt xdir = cos(rad);
-			Flt ydir = sin(rad);
-			b->pos[0] += xdir*20.0f;
-			b->pos[1] += ydir*20.0f;
-			b->vel[0] += xdir*6.0f + rnd()* 0.1;
-			b->vel[1] += ydir*6.0f + rnd()* 0.1;
-			b->color[0] = 1.0f;
-			b->color[1] = 1.0f;
-			b->color[2] = 1.0f;
-			g.nbullets++;
-		}
-	}
+    //move shoot
+    cout << "space bar" << endl;
+    struct timespec bt;
+    clock_gettime(CLOCK_REALTIME, &bt);
+    double ts = timeDiff(&g.bulletTimer, &bt);
+    Bullet *b = &g.barr[g.nbullets];
+    if (ts > 0.1) {
+        timeCopy(&g.bulletTimer, &bt);
+        if (g.nbullets < MAX_BULLETS) {
+            timeCopy(&b->time, &bt);
+            b->pos[0] = g.ship.pos[0];
+            b->pos[1] = g.ship.pos[1];
+            b->vel[0] = g.ship.vel[0];
+            b->vel[1] = g.ship.vel[1];
+            Flt rad = ((g.ship.angle+90.0)/ 360.0f) * PI * 2.0;
+            Flt xdir = cos(rad);
+            Flt ydir = sin(rad);
+            b->pos[0] += xdir*20.0f;
+            b->pos[1] += ydir*20.0f;
+            b->vel[0] += xdir*6.0f + rnd()* 0.1;
+            b->vel[1] += ydir*6.0f + rnd()* 0.1;
+            b->color[0] = 1.0f;
+            b->color[1] = 1.0f;
+            b->color[2] = 1.0f;
+            g.nbullets++;
+        }
+    }
 }
 
 void physics() {
