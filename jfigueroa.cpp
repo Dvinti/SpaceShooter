4bc25f954fe250961caebd0ba8eb55ea8424eb4b
JoseFigueroa21 <75599007+JoseFigueroa21@users.noreply.github.com>
2021-12-02 03:40:07 -0800

    Update jfigueroa.cpp

--- jfigueroa.cpp
+++ jfigueroa.cpp
@@ -157,5 +157,5 @@ void show_jose_credits(int x, int y)
     r.bot = y;
     r.left = x;
     r.center = 1;
-    ggprint8b(&r, 16, 0x00ff0000, "Jose Figueroa");
+    ggprint40(&r, 16, 0x00ff0000, "Jose Figueroa");
 }
